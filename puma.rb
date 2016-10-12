1c04f74a81e88acef1f2fb37d7518bc8707e9f87
Jason Lee <huacnlee@gmail.com>
2016-10-12 14:38:27 +0800

    Update puma config

--- puma.rb
+++ puma.rb
@@ -1,6 +1,7 @@
 threads ENV.fetch('MIN_THREADS', 1), ENV.fetch('MAX_THREADS', 1)
 port ENV['PORT'] if ENV['PORT']
 workers ENV.fetch('WORKER_COUNT', 1)
+daemonize ENV['DAEMONIZE']
 
 require 'bundler_api/metriks'
 BundlerApi::Metriks.start
