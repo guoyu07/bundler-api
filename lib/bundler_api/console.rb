106322e9d230897ef883f4bd8dbfc634c461db68
Jason Lee <huacnlee@gmail.com>
2016-10-12 14:36:17 +0800

    Revert "Add db_helper for console"
    This reverts commit 77d501242acd364eb5c16f076ce5a85959642d67.

--- lib/bundler_api/console.rb
+++ lib/bundler_api/console.rb
@@ -6,13 +6,6 @@ module BundlerApi
     def db
       @db ||= Sequel.connect(ENV['DATABASE_URL'], max_connections: 5)
     end
-
-    def db_helper
-      return @db_helper if defined? @db_helper
-      gem_cache = BundlerApi::CacheInvalidator.new
-      mutex     = Mutex.new
-      @db_helper = BundlerApi::GemDBHelper.new(db, gem_cache, mutex)
-    end
   end
 
   class Console
